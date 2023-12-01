9cf686aa9e04be6369eb62563e29503b220d5be6
jose gabriel <jgabriel2608@gmail>
2023-12-01 18:07:47 -0500

    nuevos cambios

---
+++ consulta002/ejercicio1/main.py
@@ -0,0 +1,17 @@
+# This is a sample Python script.
+
+# Press Shift+F6 to execute it or replace it with your code.
+
+
+def print_hi(name):
+    print(f'Hi, {name}')
+
+
+if __name__ == '__main__':
+    print_hi('Netbeans')
+    
+    numero = int(input("Ingresa un número: "))
+if numero % 2 == 0:
+    print("El número es par.")
+else:
+    print("El número es impar.")
