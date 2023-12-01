9c9c29b36af1c853190ff630961ee3b5f3956362
jose gabriel <jgabriel2608@gmail>
2023-12-01 18:03:41 -0500

    nuevos cambios

---
+++ consulta001/ejercio2/main.py
@@ -0,0 +1,15 @@
+# This is a sample Python script.
+
+# Press Shift+F6 to execute it or replace it with your code.
+
+
+def print_hi(name):
+    print(f'Hi, {name}')
+
+
+if __name__ == '__main__':
+    print_hi('Netbeans')
+nombre = input("Ingresa tu nombre: ")
+
+# Imprimir el dato ingresado
+print(  nombre )
