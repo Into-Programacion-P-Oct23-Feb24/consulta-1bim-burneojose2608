049233727cf8105866d2c20acdcc3fa838b6adec
jose gabriel <jgabriel2608@gmail>
2023-12-01 18:11:47 -0500

    nuevos cambios

---
+++ consulta002/ejercicio2/main.py
@@ -0,0 +1,16 @@
+# This is a sample Python script.
+
+# Press Shift+F6 to execute it or replace it with your code.
+
+
+def print_hi(name):
+    print(f'Hi, {name}')
+
+
+if __name__ == '__main__':
+    print_hi('numeros')
+contador = 10
+
+while contador > 5:
+    print(contador)
+    contador -= 1
